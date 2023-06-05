5c66d2bbadc22d655e4077300f3e788a83ea0994
evan baron <egbaron@gmail.com>
2023-06-05 17:27:26 -0600

    added javascript and home paragraph

---
+++ script.js
@@ -0,0 +1,117 @@
+let project = [
+    {
+        name: 'Will',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo ante, fermentum sit amet metus et, malesuada semper elit. Integer ipsum mauris, volutpat ac tellus nec, commodo scelerisque libero. Aliquam in magna mauris. Cras consectetur urna sed vestibulum egestas. Duis dictum quam eget pharetra maximus.',
+        links: [
+            {
+                title: 'View Site',
+                link: '#'
+            },
+            {
+                title: 'Github',
+                link: 'https://github.com/evan-baron/portfolio-website'
+            }
+        ]
+    },
+    {
+        name: 'Update',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo ante, fermentum sit amet metus et, malesuada semper elit. Integer ipsum mauris, volutpat ac tellus nec, commodo scelerisque libero. Aliquam in magna mauris. Cras consectetur urna sed vestibulum egestas.',
+        links: [
+            {
+                title: 'View Site',
+                link: '#'
+            }
+        ]
+    },
+    {
+        name: 'As I',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo ante, fermentum sit amet metus et, malesuada semper elit. Integer ipsum mauris, volutpat ac tellus nec, commodo scelerisque libero. Aliquam in magna mauris.',
+        links: [
+            {
+                title: 'View Site',
+                link: '#'
+            },
+            {
+                title: 'Github',
+                link: 'https://github.com/evan-baron/portfolio-website'
+            },
+            {
+                title: 'See in Google Play Store',
+                link: 'https://play.google.com/store/games'
+            }
+        ]
+    },
+    {
+        name: 'Build More',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo ante, fermentum sit amet metus et, malesuada semper elit. Integer ipsum mauris, volutpat ac tellus nec, commodo scelerisque libero. Aliquam in magna mauris. Cras consectetur urna sed vestibulum egestas. Duis dictum quam eget pharetra maximus.',
+        links: [
+            {
+                title: 'View Site',
+                link: '#'
+            },
+            {
+                title: 'Github',
+                link: 'https://github.com/evan-baron/portfolio-website'
+            }
+        ]
+    },
+    {
+        name: 'Websites',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo ante, fermentum sit amet metus et, malesuada semper elit. Integer ipsum mauris, volutpat ac tellus nec, commodo scelerisque libero. Aliquam in magna mauris. Cras consectetur urna sed vestibulum egestas. Duis dictum quam eget pharetra maximus.',
+        links: [
+            {
+                title: 'View Site',
+                link: '#'
+            },
+            {
+                title: 'Github',
+                link: 'https://github.com/evan-baron/portfolio-website'
+            }
+        ]
+    }
+];
+
+for (let i = 0; i < project.length; i++) {
+    document.getElementById('lc-'+[i + 1]).innerHTML = project[i].name;
+}
+
+function projectShow(projNum) {
+    let projectTitle = document.getElementById('project-title')
+    let projectContent = document.getElementById('project-content')
+
+    projectTitle.style.fontSize = '2rem';
+
+    let linkArr = document.getElementsByClassName('link');
+    for (let i = 0; i < linkArr.length; i++) {
+        linkArr[i].style.display = 'none';
+    }
+
+    for (let i = 0; i < project.length; i++) {
+        if (projNum - 1 == i) {
+            projectTitle.innerHTML = project[i].name;
+            projectContent.innerHTML = project[i].content;
+            console.log(project[i].links.length);
+            for (let j = 0; j < project[i].links.length; j++) {
+                let linkTitle = document.getElementById('link-'+(j+1));
+                linkTitle.innerHTML = project[i].links[j].title;
+                let pLink = document.getElementById('plink-'+(j+1));
+                document.getElementById('plink-row').style.display = "flex";
+                pLink.style.display = 'inline-block';
+            }
+        }
+    }
+}
+
+function home() {
+    let linkArr = document.getElementsByClassName('link');
+    for (let i = 0; i < linkArr.length; i++) {
+        linkArr[i].style.display = 'none';
+    }
+
+    document.getElementById('project-title').innerHTML = 'Welcome';
+    document.getElementById('project-content').innerHTML = `Hello! My name is Evan Baron and I am a software engineer! I started my career in sales and have a rich understanding of how to work with clients, organize projects, and meet deadlines. I am transitioning out of sales into software engineering and this is my portfolio page!<br><br>Thanks for stopping by!<br>
+    <br>
+    <div class="sig"></div>
+    Evan Baron`;
+    document.getElementById('project-title').style.fontSize = '3rem';
+}
